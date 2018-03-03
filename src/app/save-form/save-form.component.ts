4edc2966f36a2c50e18608282b2fa8501c61a21f
Jurgis Šidlauskas <sidlauskas.jurgis@gmail.com>
2018-03-03 17:11:18 +0200

    created post form for equipment

--- src/app/save-form/save-form.component.ts
+++ src/app/save-form/save-form.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 
 import {Iranga} from '../iranga';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-save-form',
@@ -9,12 +10,31 @@ import {Iranga} from '../iranga';
 })
 export class SaveFormComponent {
 
+  constructor(private http: HttpClient) {
+  }
+
   model = new Iranga('aa', 'Dr IQ', 5, '2018-02-15');
 
   submitted = false;
 
   onSubmit() {
     this.submitted = true;
+    console.log('subas na');
+
+    const req = this.http.post('http://cors-anywhere.herokuapp.com/vgtu-warehouse-api.herokuapp.com/warehouse/equipment', {
+      name: this.model.name,
+      description: this.model.description,
+      quantity: this.model.quantity,
+      purchaseDate: this.model.purchaseDate
+    })
+      .subscribe(
+        res => {
+          console.log(res);
+        },
+        err => {
+          console.log('Error occured');
+        }
+      );
   }
 
   // TODO: Remove this when we're done
