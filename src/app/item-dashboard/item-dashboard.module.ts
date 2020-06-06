a1eded217eeb19f505c2a80710d0b3f228116e38
Jurgis Sidlauskas <sidlauskas.jurgis@gmail.com>
2020-06-06 22:31:33 +0000

    implement initial item detail

--- src/app/item-dashboard/item-dashboard.module.ts
+++ src/app/item-dashboard/item-dashboard.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ItemDashboardComponent } from './containers/item-dashboard/item-dashboard.component';
+import { ItemDetailComponent } from './components/item-detail/item-detail.component';
 
 @NgModule({
     declarations:[
-      ItemDashboardComponent
+      ItemDashboardComponent,
+      ItemDetailComponent
     ],
     imports: [
         CommonModule
