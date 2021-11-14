dcd142491ab1222fbb46277953b250590f665279
Jurgis Sidlauskas <sidlauskas.jurgis@gmail.com>
2021-11-14 19:33:54 +0200

    feat: setup basic layout

---
+++ src/app/item-register/item-register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-register',
+  templateUrl: './item-register.component.html',
+  styleUrls: ['./item-register.component.scss']
+})
+export class ItemRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
