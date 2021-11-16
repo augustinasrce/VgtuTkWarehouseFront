ad13f32b83056d3ed5612e77646271b4ecc66b09
Jurgis Sidlauskas <sidlauskas.jurgis@gmail.com>
2021-11-16 23:23:05 +0200

    feat: create items table

--- src/app/item-dashboard/containers/item-dashboard/item-dashboard.component.ts
+++ src/app/item-dashboard/containers/item-dashboard/item-dashboard.component.ts
@@ -1,72 +1,79 @@
-import { Component, OnInit } from "@angular/core";
-import { Item } from "../../models/item.interface";
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import { Component } from '@angular/core';
+import { Item } from '../../models/item.interface';
 
 @Component({
-  selector: "item-dashboard",
-  styleUrls: ["item-dashboard.component.scss"],
-  template: `
-    <div>
-      item-dashboard
-      <item-detail
-        *ngFor="let item of items"
-        [detail]="item"
-        (edit)="handleEdit($event)"
-        (delete)="handleDelete($event)"
-        >
-      </item-detail>
-    </div>
-  `,
+  selector: 'app-item-dashboard',
+  styleUrls: ['item-dashboard.component.scss'],
+  templateUrl: './item-dashboard.component.html',
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition(
+        'expanded <=> collapsed',
+        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)'),
+      ),
+    ]),
+  ],
 })
 export class ItemDashboardComponent {
   items: Item[];
+  columnsToDisplay = ['brand', 'model', 'description', 'quantity'];
 
   constructor() {
     this.items = [
       {
         id: 1,
-        brand: "Petzl",
-        model: "Atache",
-        description: "Crab",
+        brand: 'Petzl',
+        model: 'Atache',
+        description: 'Crab',
         quantity: 5,
       },
       {
         id: 2,
-        brand: "Petzl",
-        model: "OK",
-        description: "Crab",
+        brand: 'Petzl',
+        model: 'OK',
+        description: 'Crab',
         quantity: 4,
       },
       {
         id: 3,
-        brand: "Petzl",
-        model: "Jumar",
-        description: "Ascender",
+        brand: 'Petzl',
+        model: 'Jumar',
+        description: 'Ascender',
         quantity: 2,
       },
       {
         id: 4,
-        brand: "Petzl",
-        model: "Reverso",
-        description: "Descender",
+        brand: 'Petzl',
+        model: 'Reverso',
+        description: 'Descender',
         quantity: 1,
       },
       {
         id: 5,
-        brand: "BD",
-        model: "Spot",
-        description: "Headlight",
+        brand: 'BD',
+        model: 'Spot',
+        description: 'Headlight',
         quantity: 2,
       },
     ];
   }
 
   handleEdit(item: Item) {
-    console.log("edit");
+    console.log('edit');
     console.log(item);
   }
 
   handleDelete(item: Item) {
-    console.log("delete");
+    console.log('delete');
     console.log(item);
   }
 }
--- src/app/item-dashboard/item-dashboard.module.ts
+++ src/app/item-dashboard/item-dashboard.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ItemDashboardComponent } from './containers/item-dashboard/item-dashboard.component';
 import { ItemDetailComponent } from './components/item-detail/item-detail.component';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [ItemDashboardComponent, ItemDetailComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule],
   exports: [ItemDashboardComponent],
 })
 export class ItemDashboardModule {}
