f1faf3367018ea07de726e14174750876503ccc2
Jurgis Sidlauskas <sidlauskas.jurgis@gmail.com>
2021-11-20 23:32:45 +0200

    feat: implement register form

--- src/app/equipment/equipment-register/equipment-register-form/equipment-register-form.component.ts
+++ src/app/equipment/equipment-register/equipment-register-form/equipment-register-form.component.ts
@@ -1,12 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Equipment } from '../../models/equipment.interface';
 
 @Component({
   selector: 'app-equipment-register-form',
   templateUrl: './equipment-register-form.component.html',
   styleUrls: ['./equipment-register-form.component.scss'],
 })
-export class EquipmentRegisterFormComponent implements OnInit {
-  constructor() {}
+export class EquipmentRegisterFormComponent {
+  @Output() saveEquipmentEmmiter: EventEmitter<Equipment> =
+    new EventEmitter();
 
-  ngOnInit(): void {}
+  constructor(private formBuilder: FormBuilder) {}
+
+  registerEquipmentForm = this.formBuilder.group({
+    brand: [''],
+    model: [''],
+    description: [''],
+    quantity: [undefined],
+  });
+
+  onSubmit() {
+    if (this.registerEquipmentForm.valid) {
+      const equipment: Equipment = {
+        brand: this.registerEquipmentForm.controls['brand'].value,
+        model: this.registerEquipmentForm.controls['model'].value,
+        description:
+          this.registerEquipmentForm.controls['description'].value,
+        quantity:
+          this.registerEquipmentForm.controls['quantity'].value,
+      };
+      this.saveEquipmentEmmiter.emit(equipment);
+    }
+  }
 }
